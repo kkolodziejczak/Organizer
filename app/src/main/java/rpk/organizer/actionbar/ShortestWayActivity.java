771e3b4d1d323fc01c355e9bc11a94fee1d2a86a
Radoslaw Adamowicz <r.adamowicz4@wp.pl>
2017-04-22 12:13:36 +0200

    Dodano pusta aktywnosc do prezentacji najkrotszej drogi cz2

--- app/src/main/java/rpk/organizer/actionbar/ShortestWayActivity.java
+++ app/src/main/java/rpk/organizer/actionbar/ShortestWayActivity.java
@@ -3,14 +3,11 @@ package rpk.organizer.actionbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import rpk.organizer.actionbar.R;
-
 public class ShortestWayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shortest_way);
-        // todo...
     }
 }
