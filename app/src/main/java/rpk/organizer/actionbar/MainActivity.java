d48637e8f2d7269ee92933c0a70b26d37bb98292
Paweł Lech <pawlon.pl@gmail.com>
2017-04-22 15:54:42 +0200

    Fragment toolbar working

--- app/src/main/java/rpk/organizer/actionbar/MainActivity.java
+++ app/src/main/java/rpk/organizer/actionbar/MainActivity.java
@@ -1,6 +1,8 @@
 package rpk.organizer.actionbar;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
