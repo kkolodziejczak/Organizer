61808f8559478e9e63b827e63bc436274f337c76
Radoslaw Adamowicz <r.adamowicz4@wp.pl>
2017-04-22 10:21:18 +0200

    ddd

---
+++ app/src/main/java/rpk/organizer/actionbar/ShortestWayActivity.java
@@ -0,0 +1,15 @@
+package rpk.organizer.actionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rpk.organizer.actionbar.R;
+
+public class ShortestWayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shortest_way);
+    }
+}
