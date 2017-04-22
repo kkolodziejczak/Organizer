0eeee39f77e6769ab1f7235f14551eeb6b53982f
Paweł Lech <pawlon.pl@gmail.com>
2017-04-22 13:03:42 +0200

    Empty MyPlacesActivity

---
+++ app/src/main/java/rpk/organizer/actionbar/MyPlacesActivity.java
@@ -0,0 +1,14 @@
+package rpk.organizer.actionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MyPlacesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_places);
+    }
+}
