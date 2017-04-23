ecbee4b94c801314ba112853fd33a0eaf99fb3c5
Radoslaw Adamowicz <r.adamowicz4@wp.pl>
2017-04-23 15:51:53 +0200

    Poki co wywalono mape i sa tylko nasze wspolrzedne gps - dziala bezblednie

--- app/src/main/java/rpk/organizer/actionbar/MapsActivity.java
+++ app/src/main/java/rpk/organizer/actionbar/MapsActivity.java
@@ -1,191 +1,156 @@
+// https://github.com/klaasnotfound/LocationAssistant
+/*
+ *    Copyright 2017 Klaas Klasing (klaas [at] klaasnotfound.com)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package rpk.organizer.actionbar;
 
-import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
-import android.provider.Settings;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.util.Log;
-import android.widget.Button;
+import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+public class MapsActivity extends Activity implements LocationAssistant.Listener {
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
-    final static int PERMISSION_ALL = 1;
-    final static String[] PERMISSIONS = {android.Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION};
-    private GoogleMap mMap;
-    TextView address,location;
-    Button getLocation;
-    MarkerOptions mo;
-    Marker marker;
-    private LocationManager locationManager;
+    private TextView tvLocation;
+    private LocationAssistant assistant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        assistant = new LocationAssistant(this, this, LocationAssistant.Accuracy.HIGH, 5000, false);
+        assistant.setVerbose(true);
+
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        mo = new MarkerOptions().position(new LatLng(0, 0)).title("My Current Location");
-        if (Build.VERSION.SDK_INT >= 23 && !isPermissionGranted()) {
-            requestPermissions(PERMISSIONS, PERMISSION_ALL);
-        } else requestLocation();
-        if (!isLocationEnabled())
-            showAlert(1);
-        /*Button btnShowLocation = (Button) findViewById(R.id.getlocation);
+        tvLocation = (TextView) findViewById(R.id.tvLocation);
+        tvLocation.setText(getString(R.string.empty));
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        assistant.start();
+    }
 
-        // show location button click event
-        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+    @Override
+    protected void onPause() {
+        assistant.stop();
+        super.onPause();
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (assistant.onPermissionsUpdated(requestCode, grantResults))
+            tvLocation.setOnClickListener(null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        assistant.onActivityResult(requestCode, resultCode);
+    }
+
+    @Override
+    public void onNeedLocationPermission() {
+        tvLocation.setText("Need\nPermission");
+        tvLocation.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View arg0) {
-                tracker=new LocationTracker(MapsActivity.this);
-                // check if location is available
-                if(tracker.isLocationEnabled)
-                {
-                    double latitude=tracker.getLatitude();
-                    double longitude=tracker.getLongitude();
-                    location.setText("Your Location is Latitude= " + latitude + " Longitude= " + longitude);
-                    String addres= getCompleteAddressString(latitude,longitude);
-                    address.setText(addres);
-                }
-                else
-                {
-                    // show dialog box to user to enable location
-                    tracker.askToOnLocation();
-                }
+            public void onClick(View view) {
+                assistant.requestLocationPermission();
             }
-        });*/
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        marker = mMap.addMarker(mo);
+        });
+        assistant.requestAndPossiblyExplainLocationPermission();
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-        LatLng myCoordinates = new LatLng(location.getLatitude(), location.getLongitude());
-        marker.setPosition(myCoordinates);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(myCoordinates));
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
-
-    private void requestLocation() {
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        criteria.setPowerRequirement(Criteria.POWER_HIGH);
-        String provider = locationManager.getBestProvider(criteria, true);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(provider, 10000, 10, this);
-    }
-    private boolean isLocationEnabled() {
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private boolean isPermissionGranted() {
-        if (checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED || checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            Log.v("mylog", "Permission is granted");
-            return true;
-        } else {
-            Log.v("mylog", "Permission not granted");
-            return false;
-        }
-    }
-
-    private void showAlert(final int status) {
-        String message, title, btnText;
-        if (status == 1) {
-            message = "Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
-                    "use this app";
-            title = "Enable Location";
-            btnText = "Location Settings";
-        } else {
-            message = "Please allow this app to access location!";
-            title = "Permission access";
-            btnText = "Grant";
-        }
-        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setCancelable(false);
-        dialog.setTitle(title)
-                .setMessage(message)
-                .setPositiveButton(btnText, new DialogInterface.OnClickListener() {
-                    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void onExplainLocationPermission() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permissionExplanation)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-                        if (status == 1) {
-                            Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                            startActivity(myIntent);
-                        } else
-                            requestPermissions(PERMISSIONS, PERMISSION_ALL);
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        assistant.requestLocationPermission();
                     }
                 })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-                        finish();
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        tvLocation.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                assistant.requestLocationPermission();
+                            }
+                        });
                     }
-                });
-        dialog.show();
+                })
+                .show();
     }
 
+    @Override
+    public void onLocationPermissionPermanentlyDeclined(View.OnClickListener fromView,
+                                                        DialogInterface.OnClickListener fromDialog) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permissionPermanentlyDeclined)
+                .setPositiveButton(R.string.ok, fromDialog)
+                .show();
+    }
+
+    @Override
+    public void onNeedLocationSettingsChange() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.switchOnLocationShort)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        assistant.changeLocationSettings();
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public void onFallBackToSystemSettings(View.OnClickListener fromView, DialogInterface.OnClickListener fromDialog) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.switchOnLocationLong)
+                .setPositiveButton(R.string.ok, fromDialog)
+                .show();
+    }
+
+    @Override
+    public void onNewLocationAvailable(Location location) {
+        if (location == null) return;
+        tvLocation.setOnClickListener(null);
+        tvLocation.setText(location.getLongitude() + "\n" + location.getLatitude());
+        tvLocation.setAlpha(1.0f);
+        tvLocation.animate().alpha(0.5f).setDuration(400);
+    }
+
+    @Override
+    public void onMockLocationsDetected(View.OnClickListener fromView, DialogInterface.OnClickListener fromDialog) {
+        tvLocation.setText(getString(R.string.mockLocationMessage));
+        tvLocation.setOnClickListener(fromView);
+    }
+
+    @Override
+    public void onError(LocationAssistant.ErrorType type, String message) {
+        tvLocation.setText(getString(R.string.error));
+    }
 }
