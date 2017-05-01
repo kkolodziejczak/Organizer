a3a8c337a02c8bcc6c33abe3720a64001e632595
Radoslaw Adamowicz <r.adamowicz4@wp.pl>
2017-05-01 13:33:42 +0200

    Cofanie fragmentow cz. 2.

--- app/src/main/java/rpk/organizer/actionbar/MainActivity.java
+++ app/src/main/java/rpk/organizer/actionbar/MainActivity.java
@@ -1,18 +1,21 @@
 package rpk.organizer.actionbar;
 
-import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
+import java.util.List;
 
 import rpk.organizer.actionbar.Utils.EventList;
 
 public class MainActivity extends AppCompatActivity {
+
+    static public Class selectedFragmentClass = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        selectedFragmentClass = fragmentClass;
+        // nie odkladamy domyslnego fragmentu (menu) na stos fragmentow
         fragmentManager.beginTransaction().replace(R.id.frame, frag).commit();
         LoadDataToClasses();
     }
@@ -46,32 +51,67 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Fragment frag = null;
+        String tag = "MENU";
         Class fragmentClass;
         switch (item.getItemId()) {
             case R.id.mapAction:
                 fragmentClass = ShortestPathActivity.class;
+                tag = "SHORTEST_PATH";
                 break;
             case R.id.calendarAction:
                 fragmentClass = Calendar.class;
+                tag = "CALENDAR";
                 break;
             case R.id.myPlaceAction:
                 fragmentClass = Default.class;
+                tag = "MENU";
                 break;
             default:
                 fragmentClass = Default.class;
+                tag = "MENU";
         }
         try {
             frag = (Fragment) fragmentClass.newInstance();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.frame, frag).addToBackStack(null).commit();
+        // bedac np. na mapie nie mozemy przeladowac fragmentu map itd.
+        if (selectedFragmentClass != fragmentClass) {
+            getSupportFragmentManager().popBackStack();
+            // jezeli jestesmy w menu to nie dodajemy do fragmentu menu, aby uniknac 2-krotnego
+            // naciskania przycisku Back w celu opuszczenia aplikacji
+            if (fragmentClass != Default.class) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.frame, frag).addToBackStack(tag).commit();
+            }
+        }
+        // po kazdym wyborze z ActionBar zapisz informacje o klasie fragmentu
+        selectedFragmentClass = fragmentClass;
         return false;
     }
 
     public void LoadDataToClasses() {
         for (int i = 0; i < 15; i++)
             EventList.addEvent(new EventInfo("Ktos", "Meeting", "Kojama" + i, "8:15"));
+    }
+
+    // moze sie przydac. identyfikacja odbywa sie po tagach, ktore przypisywane sa do fragmentow w onOptionsItemSelected
+    private Fragment getCurrentFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        String fragmentTag = fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount() - 1).getName();
+        return fragmentManager.findFragmentByTag(fragmentTag);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+
+        if (count == 0) {
+            super.onBackPressed();
+        } else {
+            getSupportFragmentManager().popBackStack();
+        }
+
     }
 }
