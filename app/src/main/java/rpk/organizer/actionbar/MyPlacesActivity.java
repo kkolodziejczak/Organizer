952e63a1eb74b5825a20f6b2e39755b886f95382
Paweł Lech <pawlon.pl@gmail.com>
2017-05-13 15:15:47 +0200

    Dialog //bugged fab Google's fault

--- app/src/main/java/rpk/organizer/actionbar/MyPlacesActivity.java
+++ app/src/main/java/rpk/organizer/actionbar/MyPlacesActivity.java
@@ -1,8 +1,11 @@
 package rpk.organizer.actionbar;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 
 import rpk.organizer.actionbar.Utils.PlacesHandler;
@@ -20,6 +24,7 @@ import rpk.organizer.actionbar.Utils.PlacesHandler;
 public class MyPlacesActivity extends Fragment {
     private ListView PlacesListView;
     private Context mContext;
+    private FloatingActionButton fab;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -33,6 +38,39 @@ public class MyPlacesActivity extends Fragment {
         Bundle bundle = getArguments();
         mContext = getContext();
         PlacesListView = (ListView)getActivity().findViewById(R.id.lista);
+        fab = (FloatingActionButton)getActivity().findViewById(R.id.fapPlace);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // custom dialog
+                final Dialog dialog = new Dialog(getContext());
+                dialog.setContentView(R.layout.dialog_addplace);
+                // Custom Android Allert Dialog Title
+                dialog.setTitle("Custom Dialog Example");
+
+                Button dialogButtonCancel = (Button) dialog.findViewById(R.id.customDialogCancel);
+                Button dialogButtonOk = (Button) dialog.findViewById(R.id.customDialogOk);
+                // Click cancel to dismiss android custom dialog box
+                dialogButtonCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                // Your android custom dialog ok action
+                // Action for custom dialog ok button click
+                dialogButtonOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                        //TODO adding list element
+                    }
+                });
+
+                dialog.show();
+            }
+        });
         populate();
     }
     public void onItemClick(AdapterView<?> l, View v, int position, long id) {
