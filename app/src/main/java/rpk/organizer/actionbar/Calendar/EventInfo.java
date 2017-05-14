861b396a7089bc3e54ad7e1565f14c609ccbaadb
Krzysztof Kołodziejczak <mrthemo@gmail.com>
2017-05-14 14:34:47 +0200

    Quick Fix zły merge

--- app/src/main/java/rpk/organizer/actionbar/Calendar/EventInfo.java
+++ app/src/main/java/rpk/organizer/actionbar/Calendar/EventInfo.java
@@ -2,6 +2,7 @@ package rpk.organizer.actionbar.Calendar;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -30,17 +31,20 @@ public class EventInfo extends Fragment {
     private Context mContext;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Fragment fragment = getFragmentManager().findFragmentByTag("yourstringtag");
-        return inflater.inflate(R.layout.activity_event_info, container, false);
+        ViewDataBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.activity_event_info, container, false);
+//        return inflater.inflate(R.layout.activity_event_info, container, false);
+
+        View view = binding.getRoot();
+
+        binding.setVariable(0,Calendar.EventToDisplay);
+        return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         Bundle bundle = getArguments();
-
-//        ActivityMainBinding binding = DataBindingUtil.setContentView(this, LAYOUT);
-//        binding.setInfoAboutEvent(Calendar.EventToDisplay;);
         mContext = getContext();
     }
 
